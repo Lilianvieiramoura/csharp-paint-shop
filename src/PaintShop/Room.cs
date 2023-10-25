6f04889139aea8010d2312163130a1d9811cce07
Lílian Vieira <lilianv528@gmail.com>
2023-10-25 15:15:54 -0300

    Req 02

--- src/PaintShop/Room.cs
+++ src/PaintShop/Room.cs
@@ -3,7 +3,20 @@ namespace PaintShop;
 // 2 - Crie uma classe Room
 public class Room
 {
-    public double TotalPaintableArea;
-    public Room(Wall[] walls)
-    {}
+    public Wall[] walls { get; }
+    public double TotalPaintableArea
+    {
+        get {
+            double sumTotal = 0;
+            foreach(var wall in walls)
+            {
+                sumTotal += wall.PaintableArea;
+            }
+            return sumTotal;
+            }
+    }
+    public Room(params Wall[] walls)
+    {
+        this.walls = walls;
+    }
 }
