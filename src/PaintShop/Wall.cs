13aa186df6d57ba0bd7a6b3f3caed999e221ee13
Lílian Vieira <lilianv528@gmail.com>
2023-10-25 14:53:35 -0300

    Req 01

--- src/PaintShop/Wall.cs
+++ src/PaintShop/Wall.cs
@@ -3,11 +3,42 @@
 // 1 - Crie uma classe Wall
 public class Wall
 {
-    public double Width;
-    public double Height;
-    public double ExcludedArea;
-    public double PaintableArea;
+    double _width;
+    double _height;
+    public double Width
+    {
+        get { return _width; }
+        set {
+            if (value <= 0)
+            {
+                _width = 1;
+            } else
+            {
+                _width = value;
+            }
+        }
+    }
+    public double Height
+    {
+        get { return _height; }
+        set {
+            if (value <= 0)
+            {
+                _height = 1;
+            } else
+            {
+                _height = value;
+            }
+        }
+    }
+    public double ExcludedArea { get; set; }
+    public double PaintableArea
+    {
+        get { return (Width * Height) - ExcludedArea; }
+    }
     public Wall(double width, double height)
     {
+        Width = width;
+        Height = height;
     }
 }
